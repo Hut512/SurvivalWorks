f66c2bccb9741b7e404d78351fe6e891a850366d
EinePerson <einenperson@gmail.com>
2023-05-08 18:51:45 +0200

    Updated Main class

--- core/src/main/java/de/survivalworkers/core/SurvivalWorkers.java
+++ core/src/main/java/de/survivalworkers/core/SurvivalWorkers.java
@@ -1,13 +1,30 @@
 package de.survivalworkers.core;
 
+import de.survivalworkers.core.client.engine.Engine;
+import de.survivalworkers.core.client.engine.io.keys.HIDInput;
+import de.survivalworkers.core.client.util.ClientOptions;
+import de.survivalworkers.core.common.event.EventManager;
+import lombok.Getter;
+
 public class SurvivalWorkers {
+    @Getter
+    private final EventManager eventManager;
+    @Getter
+    private final ClientOptions clientOptions;
+    @Getter
+    private final HIDInput input;
+    @Getter
+    private final Engine engine;
 
-
-    public void start() {
-
+    public SurvivalWorkers() {
+        eventManager = new EventManager();
+        clientOptions = new ClientOptions();
+        input = new HIDInput();
+        engine = new Engine(input);
+        engine.start();
     }
 
     public static void main(String[] args) {
-        new SurvivalWorkers().start();
+        new SurvivalWorkers();
     }
 }
