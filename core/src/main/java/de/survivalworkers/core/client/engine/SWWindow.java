ec7f237d138e49246a36fae45318b6dc1eb110f5
EinePerson <einenperson@gmail.com>
2023-05-24 18:25:17 +0200

    Added Window/Windowed FullScreen and Icon support

--- core/src/main/java/de/survivalworkers/core/client/engine/SWWindow.java
+++ core/src/main/java/de/survivalworkers/core/client/engine/SWWindow.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.lwjgl.BufferUtils;
+import org.lwjgl.assimp.Assimp;
 import org.lwjgl.glfw.GLFWImage;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.system.MemoryStack;
 import org.lwjgl.system.MemoryUtil;
 import org.lwjgl.vulkan.VkInstance;
 
@@ -16,11 +19,13 @@ import java.io.Closeable;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 import java.nio.LongBuffer;
 
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.glfw.GLFWVulkan.glfwCreateWindowSurface;
 import static org.lwjgl.glfw.GLFWVulkan.glfwVulkanSupported;
+import static org.lwjgl.stb.STBImage.stbi_load;
 
 @Slf4j
 public class SWWindow implements Closeable {
@@ -33,7 +38,7 @@ public class SWWindow implements Closeable {
     @Setter
     private boolean resized;
 
-    public SWWindow(int width, int height) {
+    public SWWindow(int width, int height,boolean windowed) {
         title = "SurvivalWorks";
 
 
@@ -61,32 +66,26 @@ public class SWWindow implements Closeable {
             throw new RuntimeException("Could not create window");
         }
 
+        if(windowed) glfwMaximizeWindow(handle);
+        else glfwSetWindowPos(handle,0,0);
 
-
-        glfwMaximizeWindow(handle);
         glfwRequestWindowAttention(handle);
-        //For Icon (Icon does not work)
-        /*try {
-            BufferedImage image = ImageIO.read(new FileInputStream("core/src/main/resources/Icon.png"));
-            byte[] iconData = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-            ByteBuffer buffer = BufferUtils.createByteBuffer(iconData.length);
-            buffer.put(iconData);
-            buffer.flip();
-            GLFWImage.Buffer imgbuffer = GLFWImage.create(1);
-            GLFWImage glfwImage = GLFWImage.create().set(128,128,buffer);
-            imgbuffer.put(0,glfwImage);
-            glfwSetWindowIcon(handle, imgbuffer);
-        }catch (IOException e) {
-            log.error(e.getMessage());
-        }*/
+        try(MemoryStack stack = MemoryStack.stackPush()) {
+            IntBuffer i = stack.mallocInt(1);
+            IntBuffer j = stack.mallocInt(1);
+            IntBuffer k = stack.mallocInt(1);
+            ByteBuffer img = stbi_load("core/src/main/resources/Icon.png",i,j,k,4);
+            if(img == null)throw new RuntimeException("Could not Load Image");
+            GLFWImage glfwImg = GLFWImage.malloc();
+            glfwImg.set(i.get(),j.get(),img);
+            glfwSetWindowIcon(handle,GLFWImage.calloc(1,stack).put(0,glfwImg));
+        }
         glfwSetWindowSizeLimits(handle, width, height, width, height);
         glfwSetFramebufferSizeCallback(handle, ((handle1, width1, height1) -> resize(width1,height1)));
     }
 
     public SWWindow() {
-        this(1,1);
-
-        glfwSetWindowPos(handle,0,0);
+        this(1,1,false);
     }
     
     public void createSurface(VkInstance instance, LongBuffer pSurface) {
